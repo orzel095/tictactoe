c715825a02862f5b14a85cb6cab9074c544a65c3
Masteusz <mateusz.orlowski95@gmail.com>
2019-11-02 22:50:06 +0100

    Updates

---
+++ precache-manifest.eab58ab786263e2a10f59d154e99fe46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad5d1a0ddc6f57e2ed7067c57f78d8e9",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "ce0eb7bd03ee12ee47b8",
+    "url": "/tictactoe/static/css/main.c4706faa.chunk.css"
+  },
+  {
+    "revision": "46df55f23da39c29f557",
+    "url": "/tictactoe/static/js/2.adcb3879.chunk.js"
+  },
+  {
+    "revision": "ce0eb7bd03ee12ee47b8",
+    "url": "/tictactoe/static/js/main.750d0fdc.chunk.js"
+  },
+  {
+    "revision": "1dd1878a39f419eb6036",
+    "url": "/tictactoe/static/js/runtime-main.868a150c.js"
+  }
+]);
